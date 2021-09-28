1f05e68597113737074c9187eb13d9b75a13103e
Adeel <71775487+ade3l@users.noreply.github.com>
2021-09-28 11:06:22 +0300

    Added app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text("Hello flutter"),
-  ));
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+      ),
+     )
+    )
+  );
 }
