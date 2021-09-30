1b77e164c62dd1dcc5be430f12f3507f630a98fb
Adeel <71775487+ade3l@users.noreply.github.com>
2021-09-30 15:53:22 +0300

    Created stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  )
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Hello Flutter"),
         centerTitle: true,
         backgroundColor: Colors.deepOrange[300],
       ),
-      body:  Center(
+      body:  const Center(
         child: Text(
             "First Flutter App",
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.grey[600],
-            fontFamily: 'Ephesis',
-          )
+            style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.black,
+              fontFamily: 'Ephesis',
+            )
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
-        child: Text("Click"),
+        child: Text("Press"),
         backgroundColor: Colors.deepOrange[400] ,
       ),
-     )
-    )
-  );
+    );
+  }
 }
+
